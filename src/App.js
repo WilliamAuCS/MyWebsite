e02ca99a62e1d1baa2c57d2fe9dcf2c11a3f447f
William Au <WilliamAuCS@gmail.com>
2019-06-05 16:34:56 -0700

    Added in profile picture, and started on one button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import HeadShot from './portrait.png';
 
 class App extends Component {
   render() {
@@ -8,12 +9,25 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">William Au</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <figure className="Info">
+          <img src={HeadShot} className="Portrait"  alt="HeadShot"/>
+          <figcaption className="Description">
+            William Au <br/>
+            Computer Science
+          </figcaption>
+        </figure>
+
+        <div className="Buttons">
+          <input ID="Button" type="submit" value="My Resume" />
+        </div>
       </div>
+      
     );
   }
 }
